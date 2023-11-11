3a21384807ef981f729a4156e417c75abd9d92ca
Alona Helevei <105630380+alonahelevei@users.noreply.github.com>
2023-11-11 13:11:07 -0600

    Create Final_Assignment.py

---
+++ Final_Assignment_2.ipynb/Final_Assignment.py
@@ -0,0 +1,52 @@
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+                        height=900,
+                        title=stock,
+                        xaxis_rangeslider_visible=True)
+    fig.show()
+
+
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+
+tesla_data.head()
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data = requests.get(url).text
+
+print(html_data)
+
+soup = BeautifulSoup(html_data, 'html5lib')
+
+tesla_revenue = pd.read_html(url)[0]
+
+tesla_revenue.head()
+
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Tesla Annual Revenue (Millions of US $)'] != ""]
+
+tesla_revenue.tail()
+
+make_graph(tesla_data, tesla_revenue, 'Tesla')
